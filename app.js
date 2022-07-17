e9b37b14aae0e4e38e15e869b273ee3760344842
VanshuMehra <vanshumehra397@gmail.com>
2022-07-17 15:11:03 +0530

    registeration route completed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 
 // require database connection
 const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
 
 // execute database connection
 dbConnect();
@@ -17,4 +19,42 @@ app.get("/", (request, response, next) => {
   next();
 });
 
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
 module.exports = app;
